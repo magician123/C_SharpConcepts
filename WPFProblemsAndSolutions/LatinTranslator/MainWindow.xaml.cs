a7fcfe23ac0f790367412567e2367c1d5f6add0f
Tshepiso <timarape.gmail.com>
2020-11-18 21:03:29 +0200

    Latin Translator

---
+++ WPFProblemsAndSolutions/LatinTranslator/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace LatinTranslator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSinister_Click(object sender, RoutedEventArgs e)
+        {
+            labelSinister.Content = "left";
+
+        }
+
+        private void buttonDexter_Click(object sender, RoutedEventArgs e)
+        {
+            labelDexter.Content = "right";
+
+        }
+
+        private void buttonMedium_Click(object sender, RoutedEventArgs e)
+        {
+            labelMedium.Content = "center";
+        }
+    }
+}
